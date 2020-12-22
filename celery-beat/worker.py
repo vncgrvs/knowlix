181bda917a81ef7b76138840ea7b7a186d67d555
Vince <vincent.groves@leanix.net>
2020-12-22 12:40:03 +0100

    created crontask to delete downloaded files

--- celery-beat/worker.py
+++ celery-beat/worker.py
@@ -6,6 +6,10 @@ MONGODB = os.getenv("MONGODB")
 client = MongoClient(MONGODB)
 db = client["taskdb"]["ta"]
 
+
 @app.task()
 def clean_pptx(self):
-    #TODO: delete all entries with kwargs.downloaded = True
+    delete = db.delete_many({"kwargs.downloaded": True})
+    deleted_count = delete.deleted_count
+
+    return deleted_count
