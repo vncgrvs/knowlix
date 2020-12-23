6346a1f9f45c7a10abb9fabd1ea06b8b8dde1f13
Vince <vincent.groves@leanix.net>
2020-12-23 15:07:41 +0100

    corrected db link

--- celery-beat/tasks.py
+++ celery-beat/tasks.py
@@ -5,15 +5,27 @@ import os
 
 MONGODB = os.getenv("MONGODB")
 client = MongoClient(MONGODB)
-db = client["crontasks"]["ta"]
+db = client["tasksdb"]["ta"]
 
 
 @app.task(name="clean_pptx", bind=True)
 def clean_pptx(self):
-    # print("fired clean task", self.request.id)
+    delete_items = list()
+    db_delete_list = db.find({"kwargs.downloaded":True})
 
+    for item in db_delete_list:
+        raw=item["result"]
+        filepath=parse_filepath(raw)
+        delete_items.append(filepath)
 
     # delete = db.delete_many({"kwargs.downloaded": True})
     # deleted_count = delete.deleted_count
 
-    return "test"
+    return delete_items
+
+
+def parse_filepath(item):
+    json_item = json.loads(item)
+    filepath = json_item["filePath"]
+    
+    return filepath
