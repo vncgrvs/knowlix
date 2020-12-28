00aba4ce6e11288bfda7891cf1760e46bb4424c9
Vince <vincent.groves@leanix.net>
2020-12-29 00:44:52 +0100

    small fixes

--- frontend/nuxt-fe/pages/onboarding/modular.vue
+++ frontend/nuxt-fe/pages/onboarding/modular.vue
@@ -14,7 +14,7 @@
         <p class="text-gray-900 font-lix font-light text-6xl tracking-wide">
           Slidedeck
         </p>
-        <span class="flex items-center ring-4">
+        <span class="flex items-center ">
           <button
             type="button"
             class="inline-flex  items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
